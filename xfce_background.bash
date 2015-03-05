195c1f37d01ed21f0729eb6f0fc5982e621e8bd5
Paul Kaefer <paul.kaefer@marquette.edu>
2015-03-05 12:39:39 -0600

    comment about compatibility

--- xfce_background.bash
+++ xfce_background.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 default=$(shuf -n 1 subreddits)
 sub=${1:-$default}
 echo "today's wallpaper is from /r/"$sub
+
 pattern="http://i.imgur.com.\w{0,}.jpg"
 url=$(curl -s "http://www.reddit.com/r/$sub/top" | grep -Eo $pattern | shuf -n 1)
+
 filename=$(echo $url | grep -Eo "\w{0,}.jpg")
 wget -q $url
 mv $filename $HOME/pics/.
 
+# use echo $DESKTOP_SESSION to determine desktop used. The following works if 'xfce' is returned.
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $HOME/pics/$filename
