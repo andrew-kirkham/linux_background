d3a62a867882d712f1212484dda40f8265314c4e
Paul Kaefer <paul.kaefer@gmail.com>
2015-03-09 00:24:01 -0500

    made script work on MATE

--- xfce_background.bash
+++ xfce_background.bash
@@ -2,14 +2,29 @@
 
 default=$(shuf -n 1 subreddits)
 sub=${1:-$default}
-echo "today's wallpaper is from /r/"$sub
 
 pattern="http://i.imgur.com.\w{0,}.jpg"
 url=$(curl -s "http://www.reddit.com/r/$sub/top" | grep -Eo $pattern | shuf -n 1)
 
 filename=$(echo $url | grep -Eo "\w{0,}.jpg")
 wget -q $url
+
+if [ ! -d $HOME/pics ]
+then
+    mkdir $HOME/pics
+fi
+
 mv $filename $HOME/pics/.
 
 # use echo $DESKTOP_SESSION to determine desktop used. The following works if 'xfce' is returned.
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $HOME/pics/$filename
+if [ "$DESKTOP_SESSION" = "xfce" ];
+then
+    xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $HOME/pics/$filename
+    notify-send "Today's wallpaper is from /r/"$sub -i /usr/share/pixmaps/xfce4_xicon1.png
+elif [ "$DESKTOP_SESSION" = "mate" ];
+then
+    gsettings set org.mate.background picture-filename $HOME/pics/$filename
+    notify-send "Today's wallpaper is from /r/"$sub
+else
+    notify-send "Unsupported desktop environment."
+fi
