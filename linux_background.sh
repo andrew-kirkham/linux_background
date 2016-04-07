341dd95e3571d8c09ee89060f1db53bf308da760
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2016-04-07 10:46:32 -0500

    adding comments

--- linux_background.sh
+++ linux_background.sh
@@ -3,20 +3,26 @@
 default=$(shuf -n 1 subreddits)
 sub=${1:-$default}
 
+#find any imgur links from our chosen subreddit and pick a random one
 pattern="http://i.imgur.com.\w{0,}.jpg"
 url=$(curl -s "https://www.reddit.com/r/$sub/top" | grep -Eo $pattern | shuf -n 1)
 
+#download the image
 filename=$(echo $url | grep -Eo "\w{0,}.jpg")
 wget -q $url
 
+#make sure we have a dir to store the images
 if [ ! -d $HOME/reddit_backgrounds ]
 then
     mkdir $HOME/reddit_backgrounds
 fi
 
+#put the file into our dir for these images
 mv $filename $HOME/reddit_backgrounds/.
 background=$HOME/reddit_backgrounds/$filename
+
 # use echo $DESKTOP_SESSION to determine desktop used. If $DESKTOP_SESSION does not work, try $GDMSESSION or $XDG_CURRENT_DESKTOP
+# attempt to figure out the desktop session to change the background
 case "$DESKTOP_SESSION" in
 'xfce')
     xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $background
@@ -32,5 +38,7 @@ case "$DESKTOP_SESSION" in
     exit
 ;;
 esac
+
+#notify where the wallpaper comes from
 notify-send "Today's wallpaper is from /r/"$sub
 
